58b5eb36ec918eded9c24dfa520fd4501197a7d6
Max Mogilski <maksmogi@gmail.com>
2022-12-09 23:03:51 +0100

    Basic structure of sidebar has been created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import "./App.scss";
+import { BrowserRouter } from "react-router-dom";
+import Sidebar from "./components/filter/sidebar/Sidebar";
 
 function App() {
-	return <div>Structure</div>;
+	return (
+		<div>
+			{/* Navbar here */}
+			<BrowserRouter>
+				{/* Please change it later to styles.container */}
+				<div className="container">
+					<Sidebar />
+					{/* Content */}
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
