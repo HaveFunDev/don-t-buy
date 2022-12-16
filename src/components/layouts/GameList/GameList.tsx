c7a639c49263b7eb4a82872d107a3e720f92d727
gacmen45 <marekgacek45@gmail.com>
2022-12-16 12:05:02 +0100

    little changes in css

--- src/components/layouts/GameList/GameList.tsx
+++ src/components/layouts/GameList/GameList.tsx
@@ -1,14 +1,12 @@
 import styles from './GameList.module.scss'
 import HeroImg from './HeroImg/HeroImg'
 import GameCard from './GameCard/GameCard'
-import Test from '../../test/Test'
 
 const GameList = () => {
 	return (
 		<div className={styles.container}>
 			<HeroImg />
 			<GameCard />
-			{/* <Test/> */}
 		</div>
 	)
 }
