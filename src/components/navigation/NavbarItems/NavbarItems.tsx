6237062c4fc16ee5fcdb9e55fd2c2382b55430c1
Max Mogilski <maksmogi@gmail.com>
2022-12-23 17:19:09 +0100

    Added redux, and created basic structure for filters

--- src/components/navigation/NavbarItems/NavbarItems.tsx
+++ src/components/navigation/NavbarItems/NavbarItems.tsx
@@ -1,27 +1,28 @@
-import styles from './NavbarItems.module.scss'
-import NavbarButtons from '../NavbarButtons/NavbarButtons'
+import styles from "./NavbarItems.module.scss";
+import NavbarButtons from "../NavbarButtons/NavbarButtons";
 
 // This categories have to be dynamic
-const categories = ['Streams', 'Game Store', 'News', 'Other']
-
+const categories = ["Streams", "Game Store", "News", "Other"];
 
 interface Props {
 	handleNav: boolean;
 }
-const NavbarItems = (props:Props) => {
+const NavbarItems = (props: Props) => {
 	return (
-		<div className={`${styles.items} ${props.handleNav ? styles.active : ''}`}>
+		<div className={`${styles.items} ${props.handleNav ? styles.active : ""}`}>
 			<div className={styles.btns}>
 				<NavbarButtons />
 			</div>
-			<ul className={styles['items__list']}>
+			<ul className={styles["items__list"]}>
 				{/* This categories have to be dynamic  */}
-				{categories.map(item => (
-					<li className={styles['items__list-item']}>{item}</li>
+				{categories.map((item) => (
+					<li className={styles["items__list-item"]} key={item}>
+						{item}
+					</li>
 				))}
 			</ul>
 		</div>
-	)
-}
+	);
+};
 
-export default NavbarItems
+export default NavbarItems;
--- src/components/sidebar/check-block/select-element/SelectElement.tsx
+++ src/components/sidebar/check-block/select-element/SelectElement.tsx
@@ -1,14 +1,32 @@
+import { useRef, useState } from "react";
 import styles from "./SelectElement.module.scss";
+import { addFilter, removeFilter, store } from "../../../../store/index";
 
 interface Props {
 	name: string;
 }
 
 const SelectElement = (props: Props) => {
+	const [isChecked, setIsChecked] = useState(false);
+	const checkbox = useRef<HTMLInputElement | null>(null);
+	const changeCategory = () => {
+		if (isChecked) {
+			store.dispatch(removeFilter(props.name));
+		} else {
+			store.dispatch(addFilter(props.name));
+		}
+		setIsChecked((prev) => !prev);
+	};
+
 	return (
 		<div className={styles.container}>
 			<label>
-				<input className={styles.checkbox} type="checkbox" />
+				<input
+					className={styles.checkbox}
+					ref={checkbox}
+					onChange={changeCategory}
+					type="checkbox"
+				/>
 				<span className={styles.name}>{props.name}</span>
 			</label>
 		</div>
