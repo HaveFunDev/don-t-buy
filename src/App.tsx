29a686211a36c2ee8c6410832fea29cecb85ef3b
Max Mogilski <maksmogi@gmail.com>
2022-12-26 13:11:20 +0100

    Created cart structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navigation/Navbar";
 import Wrapper from "./components/UI/Wrapper/Wrapper";
+import Cart from "./pages/cart/Cart";
 import Home from "./pages/home/Home";
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Home />} />
+					<Route path="cart" element={<Cart />} />
 				</Routes>
 			</BrowserRouter>
 		</Wrapper>
