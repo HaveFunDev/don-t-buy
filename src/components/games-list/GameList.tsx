21a523900cd32c1096139e4b06b04225f6edd910
gacmen45 <marekgacek45@gmail.com>
2023-01-01 15:27:57 +0100

    fix bug with type problem

--- src/components/games-list/GameList.tsx
+++ src/components/games-list/GameList.tsx
@@ -1,37 +1,50 @@
-import styles from "./GameList.module.scss";
-import { useState,useEffect } from "react";
-import GameCard from "../game-card/GameCard";
+import styles from './GameList.module.scss'
+import { useState, useEffect } from 'react'
+import GameCard from '../game-card/GameCard'
 import { collection, getDocs } from 'firebase/firestore'
 import { db } from '../../firebase'
 
-
-interface Game{
-id:string;
-title:string;
-platform:string;
-price:number;
-gallery:string;
+interface Game {
+	id: string
+	title: string
+	platform: string
+	price: number
+	gallery: string
 }
 
-
 const GameList = () => {
-	const [games, setGames] = useState([])
+	const [games, setGames] = useState<Game[]>([])
 
 	const gamesCollectionRef = collection(db, 'games')
 
+	// useEffect(() => {
+	// 	const getGames = async () => {
+	// 		const data= await getDocs(gamesCollectionRef)
+	// 		setGames(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+	// 	}
+
+	// 	getGames()
+	// }, [])
 	useEffect(() => {
 		const getGames = async () => {
-			const data= await getDocs(gamesCollectionRef)
-			setGames(data.docs.map(doc => ({ ...doc.data(), id: doc.id })))
+			const data = await getDocs(gamesCollectionRef)
+			setGames(
+				data.docs.map(doc => ({
+					id: doc.id,
+					title: doc.data().title,
+					platform: doc.data().platform,
+					price: doc.data().price,
+					gallery: doc.data().gallery,
+				}))
+			)
 		}
 
 		getGames()
 	}, [])
 
-
 	return (
 		<div className={styles.container}>
-			{games.map((game:Game) => (
+			{games.map((game: Game) => (
 				<GameCard
 					key={game.id}
 					id={game.id}
@@ -42,7 +55,7 @@ const GameList = () => {
 				/>
 			))}
 		</div>
-	);
-};
+	)
+}
 
-export default GameList;
+export default GameList
